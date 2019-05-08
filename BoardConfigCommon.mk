5a89df0bd5bbc017cb4167ff53ed239619596fd6
Akash <akashniki@gmail.com>
2019-05-09 03:23:12 +0530

    sdm845-common:Add ignore neverallow flag

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -130,6 +130,7 @@ TARGET_USE_AOSP_SURFACEFLINGER := true
 
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy/generic/private \
