c66931e1f18200144f16cfd57b0ff1e79d638138
Dan Cartier <nospamdan1@gmail.com>
2020-05-11 06:29:43 -0400

    sdm845-common: Add DEX preopt for boot and system server
    Change-Id: Ifeccfd905a461d005936ae9944533bdcf9517ee9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -90,6 +90,7 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     WITH_DEXPREOPT ?= true
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
   endif
 endif
 
