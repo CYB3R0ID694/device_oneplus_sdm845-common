691009ab2f818086de13b214fd929e51c58bb77c
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-28 03:29:01 +0000

    [SQUASH]sdm845-common: Cleanup! Revert "sdm845-common: init: workaround SafetyNet check"
    This reverts commit 9e7ccc47b1680601719d43e506f5eec73f0f5ba1.
    
    Revert "sdm845-common: Build signapk jar"
    
    This reverts commit 07708ccf6d0c1eef17d0d8c052bfd28e35824a03.
    
    Revert "sdm845-common: Add support for Pocket Judge by AOSPA"
    
    This reverts commit 288a2ac7e9aa305c5cb5c8b8c3e45bda4b13d7e8.
    
    * Move Camera provider service to configs/camera
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- init/init_sdm845.cpp
+++ init/init_sdm845.cpp
@@ -29,8 +29,6 @@
 
 #include <stdlib.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/_system_properties.h>
 
 #include "property_service.h"
@@ -57,50 +55,6 @@ void property_override_dual(char const system_prop[], char const vendor_prop[], 
 {
 	property_override(system_prop, value);
 	property_override(vendor_prop, value);
-}
-
-          /* From Magisk@jni/magiskhide/hide_utils.c */
-static const char *snet_prop_key[] = {
-    "ro.boot.vbmeta.device_state",
-    "ro.boot.verifiedbootstate",
-    "ro.boot.flash.locked",
-    "ro.boot.selinux",
-    "ro.boot.veritymode",
-    "ro.boot.warranty_bit",
-    "ro.warranty_bit",
-    "ro.debuggable",
-    "ro.secure",
-    "ro.build.type",
-    "ro.build.tags",
-    "ro.build.selinux",
-    NULL
-};
-
- static const char *snet_prop_value[] = {
-    "locked",
-    "green",
-    "1",
-    "enforcing",
-    "enforcing",
-    "0",
-    "0",
-    "0",
-    "1",
-    "user",
-    "release-keys",
-    "1",
-    NULL
-};
-
- static void workaround_snet_properties() {
-
-     // Hide all sensitive props
-    for (int i = 0; snet_prop_key[i]; ++i) {
-        property_override(snet_prop_key[i], snet_prop_value[i]);
-    }
-
-     chmod("/sys/fs/selinux/enforce", 0640);
-    chmod("/sys/fs/selinux/policy", 0440);
 }
 
 void load_dalvikvm_properties()
@@ -136,6 +90,4 @@ void vendor_load_properties()
        // Property Overrides
        property_override("ro.control_privapp_permissions", "log");
 
-       // Workaround SafetyNet
-       workaround_snet_properties();
 }
