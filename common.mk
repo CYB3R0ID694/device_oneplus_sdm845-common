77f93cc361f280717dba36c40e23d6e45f1c27d8
CYB3R0ID694 <sami.mhatre756@gmail.com>
2021-02-09 00:31:08 +0530

    sdm845-common: Bring up dot-5.0
    Signed-off-by: CYB3R0ID694 <sami.mhatre756@gmail.com>

--- common.mk
+++ common.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 
 # Inherit packages from vendor/addons/oneplus/apps
-$(call inherit-product, vendor/addons/oneplus/apps/config.mk)
+#$(call inherit-product, vendor/addons/oneplus/apps/config.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
