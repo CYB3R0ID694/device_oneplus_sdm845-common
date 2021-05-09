77f9345268ed0d4e14471337cd5f3d38516734b2
Anirudh Gupta <anirudhgupta109@gmail.com>
2021-05-09 12:50:21 +0530

    sdm845-common: DeviceSettings: Update VendorSupport library name

--- DeviceSettings/Android.mk
+++ DeviceSettings/Android.mk
@@ -30,7 +30,7 @@ LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
     androidx.preference_preference
-    VendorSupport-preference
+    VendorSupport-Libs
 #    airbnb-lottie
 
 #LOCAL_JAVA_LIBRARIES := \
