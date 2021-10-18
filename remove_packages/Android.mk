0a1bc3966694640cdee8d01ae08eb6ce942f24a4
Primus <18100664+optimus9811@users.noreply.github.com>
2021-10-18 10:22:01 +0530

    Drop Some useless GApps
    Signed-off-by: optimus<vikasyadivanshi@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,9 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := \
-    Snap \
-    Camera2
+LOCAL_OVERRIDES_PACKAGES := Snap Camera2 SafetyHubPrebuilt ScribePrebuilt TipsPrebuilt 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
