d374b59581f7e4f141bba1a570a5187aad0c5f02
CYB3R0ID694 <sami.mhatre756@gmail.com>
2022-01-15 02:17:19 +0530

    sdm845-common: Adapt for dot
    Signed-off-by: CYB3R0ID694 <sami.mhatre756@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,9 +53,9 @@ NEED_KERNEL_MODULE_SYSTEM := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := primus_defconfig
-TARGET_KERNEL_CLANG_VERSION := r437112
-TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+TARGET_KERNEL_CONFIG := sdm845-perf_defconfig
+#TARGET_KERNEL_CLANG_VERSION := r437112
+#TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
